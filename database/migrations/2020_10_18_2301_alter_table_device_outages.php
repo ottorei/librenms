e7fe425fb9cb54e5cecde47d73d870cfa0fa326a
ottorei <otto@ottorei.fi>
2020-10-18 02:13:06 +0300

    DB Schema

---
+++ database/migrations/2020_10_18_2301_alter_table_device_outages.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterAvailabilityPercColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('device_outages', function (Blueprint $table) {
+            $table->char('outage_reason', 50)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('device_outages', function (Blueprint $table) {
+            $table->dropColumn(['outage_reason']);
+        });
+    }
+}
