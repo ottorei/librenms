cab73d792e1b7e586c50a5cc04fd4723666ea276
ottorei <otto@ottorei.fi>
2020-10-18 14:09:35 +0300

    test

--- includes/functions.php
+++ includes/functions.php
@@ -2041,10 +2041,7 @@ function device_is_up($device, $record_perf = false)
 
     // Special case where the device is still down, optional mode is on, device not in maintenance mode and has no ongoing outages
     if(($consider_maintenance && !$maintenance) && ($response['status'] == '0')) {
-        $ongoing_outages = empty(dbFetchCell('SELECT going_down FROM device_outages WHERE device_id=? AND up_again IS NULL ORDER BY going_down DESC', [$device['device_id']]));
-        if($ongoing_outages) {
-            $state_update_again = true;
-        }
+        $state_update_again = empty(dbFetchCell('SELECT going_down FROM device_outages WHERE device_id=? AND up_again IS NULL ORDER BY going_down DESC', [$device['device_id']]));
     }
 
     if ($device['status'] != $response['status'] || $device['status_reason'] != $response['status_reason'] || $state_update_again) {
