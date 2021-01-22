70742df9368273efde3ab9c45324dda30ece03b8
ottorei <otto@ottorei.fi>
2021-01-22 15:06:11 +0200

    a

--- includes/polling/isis.inc.php
+++ includes/polling/isis.inc.php
@@ -12,12 +12,12 @@
 echo("Staring isis poll");
 
 // Get circuits data
-$isis_circuits_poll = snmpwalk_cache_oid($device, 'ISIS-MIB::IsisCircTable', [], 'ISIS-MIB');
-$isis_circuits_poll = collect();
+$isis_circuits_poll = snmpwalk_cache_oid($device, 'ISIS-MIB::IsisCircTable', [], '');
+
 d_echo($isis_circuits_poll);
 var_dump($isis_circuits_poll);
 
-
+$isis_circuits_poll = collect();
 
 
 
