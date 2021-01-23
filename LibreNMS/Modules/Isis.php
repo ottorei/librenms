006ec1ccf899d3d51b7bdb3085a2e95dec230125
ottorei <otto@ottorei.fi>
2021-01-23 18:01:06 +0200

    a

--- LibreNMS/Modules/Isis.php
+++ LibreNMS/Modules/Isis.php
@@ -39,6 +39,7 @@ class Isis implements Module
      */
     public function discover(OS $os)
     {
+        echo("ISIS DISCOVERY");
         if ($os instanceof IsisDiscovery) {
             echo "\nISIS SYSTEMS: ";
             ModuleModelObserver::observe('\App\Models\IsisSystem');
