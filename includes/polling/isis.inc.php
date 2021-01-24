d65ab489195e1d7219332a15b52af546b5ab7615
ottorei <otto@ottorei.fi>
2021-01-25 00:17:15 +0200

    a

--- includes/polling/isis.inc.php
+++ includes/polling/isis.inc.php
@@ -11,52 +11,51 @@ $device_model = DeviceCache::getPrimary();
 
 // Poll data from device
 $adjacencies_poll = snmpwalk_cache_oid($device, 'ISIS-MIB::isisISAdj', [], 'ISIS-MIB');
-
 $adjacencies = collect();
 
-#var_dump($isis_adjs);
+$isis_data = [];
 
 // Index the data by ifIndex
 $tmp_adjacencies = [];
-foreach ($isis_adjs as $key => $value) {
+foreach ($adjacencies_poll as $key => $value) {
     $index = explode(".", $key)[0];
     $tmp_adjacencies[$index][] = $value;
 }
 
 // Loop through all adjacencies and output their status
-foreach ($tmp_adjs as $key => $value) {
+foreach ($tmp_adjacencies as $key => $value) {
     #var_dump($value);
-    $isisISAdjState = $value[0]['isisISAdjState'];
-    $isisISAdjNeighSysType = $value[0]['isisISAdjNeighSysType'];
-    $isisISAdjNeighSysID = $value[0]['isisISAdjNeighSysID'];
-    $isisISAdjNeighPriority = $value[0]['isisISAdjNeighPriority'];
-    $isisISAdjLastUpTime = $value[0]['isisISAdjLastUpTime'];
-    $isisISAdjAreaAddress = $value[1]['isisISAdjAreaAddress'];
-    $isisISAdjIPAddrType = $value[1]['isisISAdjIPAddrType'];
-    $isisISAdjIPAddrAddress = IP::fromHexString($value[1]['isisISAdjIPAddrAddress']);
+    $isis_data["isisISAdjState"] = $value[0]['isisISAdjState'];
+    $isis_data["isisISAdjNeighSysType"] = $value[0]['isisISAdjNeighSysType'];
+    $isis_data["isisISAdjNeighSysID"] = $value[0]['isisISAdjNeighSysID'];
+    $isis_data["isisISAdjNeighPriority"] = $value[0]['isisISAdjNeighPriority'];
+    $isis_data["isisISAdjLastUpTime"] = $value[0]['isisISAdjLastUpTime'];
+    $isis_data["isisISAdjAreaAddress"] = $value[1]['isisISAdjAreaAddress'];
+    $isis_data["isisISAdjIPAddrType"] = $value[1]['isisISAdjIPAddrType'];
+    $isis_data["isisISAdjIPAddrAddress"] = IP::fromHexString($value[1]['isisISAdjIPAddrAddress']);
 
-    echo "\nisisISAdjState: " . $isisISAdjState;
-    echo "\nisisISAdjNeighSysType: " . $isisISAdjNeighSysType;
-    echo "\nisisISAdjNeighSysID: " . $isisISAdjNeighSysID;
-    echo "\nisisISAdjNeighPriority: " . $isisISAdjNeighPriority;
-    echo "\nisisISAdjLastUpTime: " . $isisISAdjLastUpTime;
-    echo "\nisisISAdjAreaAddress: " . $isisISAdjAreaAddress;
-    echo "\nisisISAdjIPAddrType: " . $isisISAdjIPAddrType;
-    echo "\nisisISAdjIPAddrAddress: " . $isisISAdjIPAddrAddress;
+    echo "\nisisISAdjState: " . $isis_data["isisISAdjState"];
+    echo "\nisisISAdjNeighSysType: " . $isis_data["isisISAdjNeighSysType"];
+    echo "\nisisISAdjNeighSysID: " . $isis_data["isisISAdjNeighSysID"];
+    echo "\nisisISAdjNeighPriority: " . $isis_data["isisISAdjNeighPriority"];
+    echo "\nisisISAdjLastUpTime: " . $isis_data["isisISAdjLastUpTime"];
+    echo "\nisisISAdjAreaAddress: " . $isis_data["isisISAdjAreaAddress"];
+    echo "\nisisISAdjIPAddrType: " . $isis_data["isisISAdjIPAddrType"];
+    echo "\nisisISAdjIPAddrAddress: " . $isis_data["isisISAdjIPAddrAddress"];
     echo "\n";
 
     // Save data
     $adjacencencies->push(new IsisAdjacency([
         'device_id' => $device['device_id'],
-        'port_id' = > $port_id,
-        'isisISAdjState' => ,
-        'isisISAdjNeighSysType',
-        'isisISAdjNeighSysID',
-        'isisISAdjNeighPriority',
-        'isisISAdjLastUpTime', $adj[0]['']
-        'isisISAdjAreaAddress',
-        'isisISAdjIPAddrType',
-        'isisISAdjIPAddrAddress',
+        'port_id' => $port_id,
+        'isisISAdjState' => $isis_data["isisISAdjState"],
+        'isisISAdjNeighSysType' => $isis_data["isisISAdjNeighSysType"],
+        'isisISAdjNeighSysID' => $isis_data["isisISAdjNeighSysID"],
+        'isisISAdjNeighPriority' => $isis_data["isisISAdjNeighPriority"],
+        'isisISAdjLastUpTime' => $isis_data["isisISAdjLastUpTime"],
+        'isisISAdjAreaAddress' => $isis_data["isisISAdjAreaAddress"],
+        'isisISAdjIPAddrType' => $isis_data["isisISAdjIPAddrType"],
+        'isisISAdjIPAddrAddress' => $isis_data["isisISAdjIPAddrAddress"],
     ]));
 }
 
@@ -70,8 +69,8 @@ foreach ($tmp_adjs as $key => $value) {
 echo PHP_EOL;
 
 unset(
-    $isis_circs,
-    $isis_adjs,
-    $tmp_adjs,
-    $adjs
+    $$adjacencies_poll
+    $adjacencies
+    $isis_data
+    $tmp_adjacencies
 );
