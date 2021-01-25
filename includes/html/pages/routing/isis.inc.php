e8968c7c3ac608b8d4d4a46f085344ba1cf8686c
ottorei <otto@ottorei.fi>
2021-01-25 17:39:01 +0200

    test

--- includes/html/pages/routing/isis.inc.php
+++ includes/html/pages/routing/isis.inc.php
@@ -32,7 +32,12 @@ foreach (dbFetchRows('SELECT A.`device_id`, A.`port_id`, A.`isisISAdjIPAddrAddre
           <tr>
             <td></td>
             <td>' . generate_device_link($device, 0, ['tab' => 'routing', 'proto' => 'isis']) . '</td>
-            <td><a href="' . generate_url(['page'=>'device', 'device'=>$adj['device_id'], 'tab'=>'port', 'port'=>$adj['port_id']]) . '">' . 'text' . '</a></td>
+            <td><a href="' . generate_url([
+                'page'=>'device', 
+                'device'=>$adj['device_id'], 
+                'tab'=>'port', 
+                'port'=>$adj['port_id'
+            ]]) . '">' . 'text' . '</a></td>
             <td>' . $adj['isisISAdjIPAddrAddress'] . '</td>
             <td>' . $adj['isisISAdjNeighSysID'] . '</td>
             <td>' . $adj['isisISAdjNeighSysType'] . '</td>
