8dc711408eba6872c552ca5c1c99221770e312ae
ottorei <otto@ottorei.fi>
2021-01-28 15:40:00 +0200

    Permissions

--- includes/html/pages/routing/isis.inc.php
+++ includes/html/pages/routing/isis.inc.php
@@ -3,56 +3,61 @@
 use App\Models\IsisAdjacency;
 use App\Models\Port;
 
-echo '
-<div>
-  <div class="panel panel-default">
-    <div class="panel-body">
-      <table class="table table-condensed table-hover" style="border-collapse:collapse;">
-        <thead>
+if (! Auth::user()->hasGlobalRead()) {
+  include 'includes/html/error-no-perm.inc.php';
+} else {
+
+  echo '
+  <div>
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <table class="table table-condensed table-hover" style="border-collapse:collapse;">
+          <thead>
+            <tr>
+              <th>&nbsp;</th>
+              <th>Local device</th>
+              <th>Local interface</th>
+              <th>Adjacent</th>
+              <th>System ID</th>
+              <th>Area</th>
+              <th>System type</th>
+              <th>State</th>
+              <th>Last uptime</th>
+            </tr>
+          </thead>';
+
+  foreach (IsisAdjacency::with('port')->get() as $adj) {
+      $device = device_by_id_cache($adj->device_id);
+      if ($adj->isisISAdjState == 'up') {
+          $color = 'green';
+      } else {
+          $color = 'red';
+      }
+
+      $interface_name = $adj->port->ifName;
+
+      echo '
+          <tbody>
           <tr>
-            <th>&nbsp;</th>
-            <th>Local device</th>
-            <th>Local interface</th>
-            <th>Adjacent</th>
-            <th>System ID</th>
-            <th>Area</th>
-            <th>System type</th>
-            <th>State</th>
-            <th>Last uptime</th>
+              <td></td>
+              <td>' . generate_device_link($device, 0, ['tab' => 'routing', 'proto' => 'isis']) . '</td>
+              <td><a href="' . generate_url([
+          'page'=>'device',
+          'device'=>$adj->device_id,
+          'tab'=>'port',
+          'port'=>$adj->port_id,
+      ]) . '">' . $interface_name . '</a></td>
+              <td>' . $adj->isisISAdjIPAddrAddress . '</td>
+              <td>' . $adj->isisISAdjNeighSysID . '</td>
+              <td>' . $adj->isisISAdjAreaAddress . '</td>
+              <td>' . $adj->isisISAdjNeighSysType . '</td>
+              <td><strong><span style="color: ' . $color . ';">' . $adj->isisISAdjState . '</span></strong></td>
+              <td>' . formatUptime($adj->isisISAdjLastUpTime) . '</td>
           </tr>
-        </thead>';
-
-foreach (IsisAdjacency::with('port')->get() as $adj) {
-    $device = device_by_id_cache($adj->device_id);
-    if ($adj->isisISAdjState == 'up') {
-        $color = 'green';
-    } else {
-        $color = 'red';
-    }
-
-    $interface_name = $adj->port->ifName;
-
-    echo '
-        <tbody>
-        <tr>
-            <td></td>
-            <td>' . generate_device_link($device, 0, ['tab' => 'routing', 'proto' => 'isis']) . '</td>
-            <td><a href="' . generate_url([
-        'page'=>'device',
-        'device'=>$adj->device_id,
-        'tab'=>'port',
-        'port'=>$adj->port_id,
-    ]) . '">' . $interface_name . '</a></td>
-            <td>' . $adj->isisISAdjIPAddrAddress . '</td>
-            <td>' . $adj->isisISAdjNeighSysID . '</td>
-            <td>' . $adj->isisISAdjAreaAddress . '</td>
-            <td>' . $adj->isisISAdjNeighSysType . '</td>
-            <td><strong><span style="color: ' . $color . ';">' . $adj->isisISAdjState . '</span></strong></td>
-            <td>' . formatUptime($adj->isisISAdjLastUpTime) . '</td>
-        </tr>
-        </tbody>';
-}
-echo '</table>
+          </tbody>';
+  }
+  echo '</table>
+      </div>
     </div>
-  </div>
-</div>';
+  </div>';
+}
