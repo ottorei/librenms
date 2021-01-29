fc17561d8978cae92502cf143d86b3b30d69894a
ottorei <otto@ottorei.fi>
2021-01-29 13:02:11 +0200

    Table

--- includes/html/pages/routing/isis.inc.php
+++ includes/html/pages/routing/isis.inc.php
@@ -76,7 +76,7 @@ if (! Auth::user()->hasGlobalRead()) {
             </tr>
           </thead>';
 
-    foreach (IsisAdjacency::where('isisISAdjState', $filter)->with('port')->get() as $adj) {
+    foreach (IsisAdjacency::where('isisISAdjState', [$filter])->with('port')->get() as $adj) {
         $device = device_by_id_cache($adj->device_id);
         if ($adj->isisISAdjState == 'up') {
             $color = 'green';
