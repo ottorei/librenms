150fb95f2e8614963fd88c2d3f4575efd3b7db8a
ottorei <otto@digipahuus.fi>
2021-08-20 22:05:20 +0300

    Syntax error

--- LibreNMS/Modules/Isis.php
+++ LibreNMS/Modules/Isis.php
@@ -125,7 +125,7 @@ class Isis implements Module
                     'isisCircAdminState' => $circuit_data['isisCircAdminState'],
                     'isisISAdjState' => $adjacency_data['isisISAdjState'] ?? $circuit_data['isisCircAdminState'],
                     'isisISAdjNeighSysType' => Arr::get($this->isis_codes, Arr::last($adjacency_data['isisISAdjNeighSysType']) ?? null, 'unknown'),
-                    'isisISAdjNeighSysID' => str_replace(' ', '.', trim($adjacency_data['isisISAdjNeighSysID'] ?? null, 'unknown'),
+                    'isisISAdjNeighSysID' => str_replace(' ', '.', trim($adjacency_data['isisISAdjNeighSysID'] ?? null, 'unknown')),
                     'isisISAdjNeighPriority' => $adjacency_data['isisISAdjNeighPriority'] ?? null, 'unknown',
                     'isisISAdjLastUpTime' => $this->parseAdjacencyTime($adjacency_data),
                     'isisISAdjAreaAddress' => str_replace(' ', '.', trim($adjacency_data['isisISAdjAreaAddress'] ?? null, 'unknown')),
